54d61713014092ae1ea200f8906b3efc40129881
raykkk <weirui.kwr@alibaba-inc.com>
2026-02-27 16:19:19 +0800

    support multi platform image

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Substitute COPAW_PORT in supervisord template and start supervisord.
 # Default port 8088; override at runtime with -e COPAW_PORT=3000.
+set -e
 export COPAW_PORT="${COPAW_PORT:-8088}"
 envsubst '${COPAW_PORT}' \
   < /etc/supervisor/conf.d/supervisord.conf.template \
