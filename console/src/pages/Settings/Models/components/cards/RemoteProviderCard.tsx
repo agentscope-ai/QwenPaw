dc0dd0cc4217b82010d4d37a2a16b95d1ab0d0e8
panxuchen.pxc <panxuchen.pxc@alibaba-inc.com>
2026-03-30 15:25:55 +0800

    finish model provider doc

--- console/src/pages/Settings/Models/components/cards/RemoteProviderCard.tsx
+++ console/src/pages/Settings/Models/components/cards/RemoteProviderCard.tsx
@@ -60,7 +60,7 @@ export function RemoteProviderCard({
 
   let isConfigured = false;
 
-  if (provider.is_local) {
+  if (provider.id === "copaw-local") {
     isConfigured = true;
   } else if (provider.is_custom && provider.base_url) {
     isConfigured = true;
