agentscope-ai/CoPaw

feat(providers): rebase wire_api + custom headers onto latest main
Port the wire_api and custom headers feature from PR #419 onto the
latest main branch, adapting to the new provider architecture where
registry.py and store.py have been replaced by provider.py and
provider_manager.py.

Changes:
- Add wire_api (chat_completions|responses) and headers fields to
  ProviderInfo with validation in update_config()
- Create OpenAIResponsesChatModel with Responses API + chat fallback
- Update OpenAIProvider to merge custom headers and select model by
  wire_api
- Extend API router and CLI with headers/wire_api options
- Mask sensitive header values in API responses and CLI output

All previous review comments addressed:
- Use AsyncOpenAI (not AsyncClient)
- Narrow fallback exception to openai.APIError
- Default stream=False (no true streaming)
- Re-raise on structured_model instead of silent fallback
- Parse output items directly, output_text only as SDK fallback
- Handle tool role + tool_call_id in _messages_to_chat

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

src/copaw/providers/provider.py
@@ -2,7 +2,7 @@
 """Definition of Provider."""
 
 from abc import ABC, abstractmethod
-from typing import Dict, List, Type, Any
+from typing import Dict, List, Literal, Type, Any
 from pydantic import BaseModel, Field
 
 from agentscope.model import ChatModelBase
@@ -60,6 +60,17 @@ class ProviderInfo(BaseModel):
     generate_kwargs: Dict[str, Any] = Field(
         default_factory=dict,
         description="Generation parameters for agentscope chat models.",
+    )
+    headers: Dict[str, str] = Field(
+        default_factory=dict,
+        description="Custom HTTP headers sent with every API request.",
+    )
+    wire_api: Literal["chat_completions", "responses"] = Field(
+        default="chat_completions",
+        description=(
+            "Which OpenAI wire format to use: "
+            "'chat_completions' (default) or 'responses'."
+        ),
     )
 
 
@@ -138,6 +149,18 @@ class Provider(ProviderInfo, ABC):
             and isinstance(config["generate_kwargs"], dict)
         ):
             self.generate_kwargs = config["generate_kwargs"]
+        if "headers" in config and config["headers"] is not None:
+            if not isinstance(config["headers"], dict):
+                raise ValueError("headers must be a dict[str, str]")
+            self.headers = config["headers"]
+        if "wire_api" in config and config["wire_api"] is not None:
+            valid = ("chat_completions", "responses")
+            if config["wire_api"] not in valid:
+                raise ValueError(
+                    f"wire_api must be one of {valid}, "
+                    f"got {config['wire_api']!r}",
+                )
+            self.wire_api = config["wire_api"]
 
     def get_chat_model_cls(self) -> Type[ChatModelBase]:
         """Return the chat model class associated with this provider."""
@@ -166,12 +189,24 @@ class Provider(ProviderInfo, ABC):
         """Return an instance of the chat model associated with this
         provider and model_id."""
 
+    @staticmethod
+    def _mask_header_value(value: str) -> str:
+        """Mask a header value for safe display."""
+        if len(value) <= 4:
+            return "*" * len(value)
+        return f"{value[:2]}***{value[-2:]}"
+
     async def get_info(self, mock_secret: bool = True) -> ProviderInfo:
         """Return a ProviderInfo instance with the provider's details."""
         api_key = (
             self.api_key_prefix + "*" * 6
             if mock_secret and self.api_key
             else self.api_key
+        )
+        masked_headers = (
+            {k: self._mask_header_value(v) for k, v in self.headers.items()}
+            if mock_secret
+            else dict(self.headers)
         )
         return ProviderInfo(
             id=self.id,
@@ -188,6 +223,8 @@ class Provider(ProviderInfo, ABC):
             freeze_url=self.freeze_url,
             require_api_key=self.require_api_key,
             generate_kwargs=self.generate_kwargs,
+            headers=masked_headers,
+            wire_api=self.wire_api,
         )
 
 

agentscope-ai/QwenPaw

src/copaw/providers/provider.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Dict, List, Type, Any
+from typing import TYPE_CHECKING, Dict, List, Literal, Type, Any
 from pydantic import BaseModel, Field
 
 from agentscope.model import ChatModelBase
@@ -95,6 +95,17 @@ class ProviderInfo(BaseModel):
         default_factory=dict,
         description="Generation parameters for agentscope chat models.",
     )
+    headers: Dict[str, str] = Field(
+        default_factory=dict,
+        description="Custom HTTP headers sent with every API request.",
+    )
+    wire_api: Literal["chat_completions", "responses"] = Field(
+        default="chat_completions",
+        description=(
+            "Which OpenAI wire format to use: "
+            "'chat_completions' (default) or 'responses'."
+        ),
+    )
 
 
 class Provider(ProviderInfo, ABC):
@@ -179,6 +190,18 @@ class Provider(ProviderInfo, ABC):
                 else ModelInfo.model_validate(model)
                 for model in config["extra_models"]
             ]
+        if "headers" in config and config["headers"] is not None:
+            if not isinstance(config["headers"], dict):
+                raise ValueError("headers must be a dict[str, str]")
+            self.headers = config["headers"]
+        if "wire_api" in config and config["wire_api"] is not None:
+            valid = ("chat_completions", "responses")
+            if config["wire_api"] not in valid:
+                raise ValueError(
+                    f"wire_api must be one of {valid}, "
+                    f"got {config['wire_api']!r}",
+                )
+            self.wire_api = config["wire_api"]
 
     def get_chat_model_cls(self) -> Type[ChatModelBase]:
         """Return the chat model class associated with this provider."""
@@ -221,12 +244,24 @@ class Provider(ProviderInfo, ABC):
 
         return ProbeResult()
 
+    @staticmethod
+    def mask_header_value(value: str) -> str:
+        """Mask a header value for safe display."""
+        if len(value) <= 4:
+            return "*" * len(value)
+        return f"{value[:2]}***{value[-2:]}"
+
     async def get_info(self, mock_secret: bool = True) -> ProviderInfo:
         """Return a ProviderInfo instance with the provider's details."""
         api_key = (
             self.api_key_prefix + "*" * 6
             if mock_secret and self.api_key
             else self.api_key
+        )
+        masked_headers = (
+            {k: self.mask_header_value(v) for k, v in self.headers.items()}
+            if mock_secret
+            else dict(self.headers)
         )
         return ProviderInfo(
             id=self.id,
@@ -246,4 +281,6 @@ class Provider(ProviderInfo, ABC):
             freeze_url=self.freeze_url,
             require_api_key=self.require_api_key,
             generate_kwargs=self.generate_kwargs,
+            headers=masked_headers,
+            wire_api=self.wire_api,
         )