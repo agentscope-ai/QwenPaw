c0cc13300e3030025eace14ebe10879d2ce63d90
saltapp <815625981@qq.com>
2026-06-05 17:42:26 +0800

    feat(console): implement customizable column order with drag-and-drop support

--- console/src/pages/Control/Sessions/components/index.ts
+++ console/src/pages/Control/Sessions/components/index.ts
@@ -1,4 +1,8 @@
-export { createColumns } from "./columns";
+export {
+  createColumns,
+  DEFAULT_SESSION_COLUMN_ORDER,
+  type SessionColumnKey,
+} from "./columns";
 export { FilterBar } from "./FilterBar";
 export { SessionDrawer } from "./SessionDrawer";
 export { formatTime, type Session } from "./constants";
